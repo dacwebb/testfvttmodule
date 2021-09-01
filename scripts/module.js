d4ff72bab1c2405f8a0ed800d953a0626aae1c1a
dacwebb <dannsenn@gmail.com>
2021-09-01 11:43:55 -0700

    Adjusted Hello World

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,8 @@
 Hooks.once('init', async function() {
+    console.log('testfvttmodule | Hello World of FVTT!');
 
 });
 
 Hooks.once('ready', async function() {
-
+    console.log('testfvttmodule | Have you seen me?!');
 });
--- scripts/testfvttmodule.js
+++
@@ -1,2 +0,0 @@
-console.log('testfvttmodule | Hello World of FVTT!');
-console.log('testfvttmodule | Have you seen me?!');
